6a4179cd905c36dc8b4a8c53a700f642e740f54a
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2019-02-03 00:12:11 +0530

    different way

--- mreco/__init__.py
+++ mreco/__init__.py
@@ -1,61 +1,52 @@
 import os
 
-from flask import Flask
-from flask import render_template
+# from . import routes, models
+from flask import Flask, render_template
 from flask_mongoengine import MongoEngine
-from flask_login import (
-    LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-)
-
-from .db import Movie
-from . import auth
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(__name__)
-    app.config['MONGODB_SETTINGS'] = {
-        'db': 'imovies',
-    }
-    app.config['TESTING'] = True
-    app.config['SECRET_KEY'] = 'wtfiswrongwithya'
-    app.debug = True
+test_config = None
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object(__name__)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'imovies',
+}
+app.config['TESTING'] = True
+app.config['SECRET_KEY'] = 'wtfiswrongwithya'
+app.debug = True
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+if test_config is None:
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile('config.py', silent=True)
+else:
+    # load the test config if passed in
+    app.config.from_mapping(test_config)
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    db = MongoEngine()
-    db.init_app(app)
+db = MongoEngine()
+db.init_app(app)
 
-    app.register_blueprint(auth.bp)
+login = LoginManager(app)
+login.login_view = 'login'
 
-    login_manager = LoginManager()
-    login_manager.init_app(app)
-    login_manager.login_view = 'login'
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.objects(pk=user_id).first()
+from mreco import routes, models
+# from mreco.models import Movie
 
-    @app.route('/')
-    def index():
-        movies = Movie.objects.all()
-        return render_template('index.html', movies=movies, user=current_user)
 
-    return app
+# @app.route('/')
+# @app.route('/index')
+# def index():
+#     movies = Movie.objects.all()
+#     return render_template('index.html', title='Home', movies=movies)
+
+if __name__ == '__main__':
+    app.run()
