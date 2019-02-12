344400e94a13883bded2ea2d60b36d1c416779a8
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2019-02-12 15:15:52 +0530

    Added dir structure

--- mreco/__init__.py
+++ mreco/__init__.py
@@ -9,12 +9,23 @@ test_config = None
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(__name__)
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'imovies',
-}
-app.config['TESTING'] = True
+
+if os.environ.get('FLASK_ENV') == 'development':
+    app.config['TESTING'] = True
+    app.debug = True
+    app.config['MONGODB_SETTINGS'] = {
+        'db': 'imovies',
+    }
+else:
+    app.config['MONGODB_DB'] = 'imovies'
+    app.config['MONGODB_HOST'] = 'mongodb://<your_mlab_username>:<your_mlab_password>@ds227255.mlab.com:27255/imovies'
+    app.config['MONGODB_PORT'] = 27255
+    app.config['MONGODB_USERNAME'] = '<your_mlab_username>'
+    app.config['MONGODB_PASSWORD'] = '<your_mlab_password>'
+    app.config['TESTING'] = False
+    app.debug = False
+
 app.config['SECRET_KEY'] = 'thisismysecret'
-app.debug = True
 
 if test_config is None:
     # load the instance config, if it exists, when not testing
